ded22ff5ee87872de9790474224d08418ff6e595
kungfupanda420 <pratheek18183@gmail.com>
2026-01-09 01:25:57 +0530

    still schema needs to be changed for roles and users and proff

--- Backend/src/main/java/com/selab/backend/models/Student.java
+++ Backend/src/main/java/com/selab/backend/models/Student.java
@@ -1,4 +1,36 @@
 package com.selab.backend.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "students")
+@Data
 public class Student {
+
+    @Id
+    private String rollNumber;
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String collegeEmailId;
+    @NotBlank
+    private String departmentName;
+
+    @NotBlank
+    private  String resumePath;
+
+
+
 }
--- Backend/src/main/java/com/selab/backend/models/User.java
+++ Backend/src/main/java/com/selab/backend/models/User.java
@@ -1,4 +1,24 @@
 package com.selab.backend.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name="users")
+@Data
 public class User {
+    @Id
+    @Column(nullable = false)
+    private Long id;
+    @NotBlank
+    @Column(nullable = false)
+    private String email;
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+    @NotBlank
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
