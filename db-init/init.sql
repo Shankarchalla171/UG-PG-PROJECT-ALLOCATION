572b95742948f93851d5249f7e004665e05c483d
kungfupanda420 <pratheek18183@gmail.com>
2026-01-16 00:53:13 +0530

    .

--- db-init/init.sql
+++
@@ -1,7 +0,0 @@
--- Only create user if it doesn't exist
-CREATE DATABASE IF NOT EXISTS student_activity_points;
-
--- Create a new user (not root)
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'Pratheek@1234';
-GRANT ALL PRIVILEGES ON seLabDatabase.* TO 'selabuser'@'%';
-FLUSH PRIVILEGES;
