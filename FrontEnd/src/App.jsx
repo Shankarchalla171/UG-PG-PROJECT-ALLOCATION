6a5914e0d5eab0c28cd2a3b1825e4f3f3cc71799
kungfupanda420 <pratheek18183@gmail.com>
2026-02-07 20:18:20 +0530

    added prof view projects

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,10 +4,11 @@ import HomePage from './pages/HomePage';
 import {Routes, Route} from 'react-router-dom';
 import ProfessorCreateProject from './pages/Professor_create_project';
 import Professor_dashboard from './pages/Professor_dashboard';
-function App() {
-  
+import ProfessorViewProjects from './pages/Professors_projects';
 
-  return (
+
+function App() {
+    return (
     <>
       
       <Routes>
@@ -15,7 +16,7 @@ function App() {
         <Route path='/home' element={<HomePage/>}/>
         <Route path='/professor_dashboard' element={<Professor_dashboard/>}/>
         <Route path='/professor_create_project' element={<ProfessorCreateProject/>}/>
-
+        <Route path='/professor_projects' element={<ProfessorViewProjects/>}/>
       </Routes>
     </>
   )
