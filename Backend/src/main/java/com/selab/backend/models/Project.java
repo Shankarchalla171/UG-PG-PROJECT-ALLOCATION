25be5e9190c2e7eb2b5cfc9bbd4c994f02b2633b
kungfupanda420 <pratheek18183@gmail.com>
2026-03-03 10:05:00 +0530

    changes in hte backend

---
+++ Backend/src/main/java/com/selab/backend/models/Project.java
@@ -0,0 +1,15 @@
+package com.selab.backend.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="project")
+@Data
+public class project {
+
+
+
+}
