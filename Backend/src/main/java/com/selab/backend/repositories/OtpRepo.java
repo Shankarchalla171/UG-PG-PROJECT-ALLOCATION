0ed181c31ca0c519a2c3e0e516f7ee7722dee1e3
kungfupanda420 <167099993+kungfupanda420@users.noreply.github.com>
2026-03-07 17:52:34 +0530

    Improved the security of OTP storage by hasing the otps before storing them

--- Backend/src/main/java/com/selab/backend/repositories/OtpRepo.java
+++ Backend/src/main/java/com/selab/backend/repositories/OtpRepo.java
@@ -3,6 +3,8 @@ package com.selab.backend.repositories;
 import com.selab.backend.models.OTPStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface OtpRepo extends JpaRepository<OTPStore, Long> {
     boolean existsByEmail(String email);
 
