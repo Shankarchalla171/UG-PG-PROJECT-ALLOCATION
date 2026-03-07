ae21e306e19a38d030cb298681f98840971aea8d
kungfupanda420 <pratheek18183@gmail.com>
2026-03-08 00:59:20 +0530

    .

--- Backend/src/main/java/com/selab/backend/controller/ProfessorController.java
+++ Backend/src/main/java/com/selab/backend/controller/ProfessorController.java
@@ -1,4 +1,26 @@
 package com.selab.backend.controller;
 
+
+import com.selab.backend.models.Professor;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/professors")
+@RequiredArgsConstructor
 public class ProfessorController {
+
+
+    @GetMapping("/profile")
+    public ResponseEntity<Professor> getProfessor(@RequestParam String email) {
+        return ResponseEntity.ok();
+    }
+
+
+
 }
