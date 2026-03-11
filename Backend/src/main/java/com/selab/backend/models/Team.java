a285fdd7b1512cc5ee08655c0a8deb0c14a4aa11
abhiram1329 <abhiram1329a@gmail.com>
2026-03-12 00:56:15 +0530

    fix:  shcema teams

--- Backend/src/main/java/com/selab/backend/models/Team.java
+++ Backend/src/main/java/com/selab/backend/models/Team.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name="teams")
@@ -11,7 +12,7 @@ import java.util.List;
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long teamId;
+    private UUID teamId;
     private String teamName;
 
     @OneToOne
