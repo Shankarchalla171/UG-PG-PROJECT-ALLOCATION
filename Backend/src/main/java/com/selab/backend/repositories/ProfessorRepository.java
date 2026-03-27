2b7adbff93ba9b5ffb3db88d1c99cc77a69a9c17
Shankarchalla171 <internshipchalla171@gmail.com>
2026-03-27 16:50:26 +0530

    fix:ui change for professor stduent request & feat: filter based on slots for faculty collaboration backend done

--- Backend/src/main/java/com/selab/backend/repositories/ProfessorRepository.java
+++ Backend/src/main/java/com/selab/backend/repositories/ProfessorRepository.java
@@ -3,10 +3,15 @@ package com.selab.backend.repositories;
 import com.selab.backend.models.Professor;
 import com.selab.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProfessorRepository extends JpaRepository<Professor,Long> {
     Optional<Professor> findByUser(User user);
     Optional<Professor> findByUserId(Long userID);
+
+    List<Professor> findAllByStudentsTakenLessThanEqual(Long allowed);
 }
