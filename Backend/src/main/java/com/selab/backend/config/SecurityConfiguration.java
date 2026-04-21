8ee7d13742f01dd7283ee6d89f6428e6fd292722
kungfupanda420 <pratheek18183@gmail.com>
2026-04-22 02:38:35 +0530

    fix:updated cors for  https and http

--- Backend/src/main/java/com/selab/backend/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/selab/backend/config/SecurityConfiguration.java
@@ -61,7 +61,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(frontendurl));
+        configuration.setAllowedOrigins(Arrays.asList("http://"+frontendurl,"https://"+frontendurl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
